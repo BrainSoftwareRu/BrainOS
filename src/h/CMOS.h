cfbd57cd7f16c6c1bcdb860ef0aff0d7b792369b
BrainSoftwareRu <92357269+BrainSoftwareRu@users.noreply.github.com>
2022-01-12 21:22:56 +0500

    Delete src directory

--- src/h/CMOS.h
+++
@@ -1,5 +0,0 @@
-#include "ports.h"
-#include "tty.h"
-
-#define CURRENT_YEAR 2021
-void get_time();
--- src/h/tty.h
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-#include <stdint.h>
-
-void t_init();
-void t_putch(char c);
-void t_print(const char* str);
-void t_putint(int i);
